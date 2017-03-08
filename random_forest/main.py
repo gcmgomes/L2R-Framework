47f73c5d637db7f5f9e381fa440eddb58c89dec8
Thiago Silva <thiagovieiraas@gmail.com>
2017-03-08 19:17:05 -0300

    Printing standard deviation now.

--- random_forest/main.py
+++ random_forest/main.py
@@ -6,6 +6,7 @@
 
 
 import sys
+from math import sqrt
 from regressor import Regressor
 
 
@@ -24,12 +25,23 @@ def main():
   random_forest = Regressor()
   random_forest.load_train_data(sys.argv[1])
   random_forest.load_test_data(sys.argv[2])
+  n_tests = 30
 
+  results = []
+  
   total = 0.0
-  for i in range(30):
+  for i in range(n_tests):
     random_forest.train(500)
-    total += random_forest.mapk(10)
-  print "MAP@10:", float(total)/30
+    results.append(random_forest.mapk(10))
+    total += results[-1]
+  
+  sd = 0
+  total = float(total)/n_tests
+  for v in results:
+    sd += (v-total)*(v-total)
+  sd = sqrt(float(sd)/n_tests)
+  
+  print "MAP@10:", total, " STDDEV: ", sd
 
 
 if __name__ == '__main__':
