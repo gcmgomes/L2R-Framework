30b9c393f2a15170af15853f164519b25d58dd13
Thiago Silva <thiagovieiraas@gmail.com>
2017-04-20 19:11:45 -0300

    Fixed the bash script to run the random forests.

--- run_random_forest.sh
+++ run_random_forest.sh
@@ -6,7 +6,7 @@ echo "Running original..."
 for i in {1..5}; do
   path="datasets/2003_td_dataset/Fold$i"
   echo "Running Fold $i..."
-  mkdir -p random_forest/predictions/original/fold${i}
+  mkdir -p random_forest/predictions/original/
   python $script_path $path/train.txt $path/test.txt &> random_forest/predictions/original/fold${i}
   echo ""
 done
@@ -17,7 +17,7 @@ echo "Running discretized tube 10 bins..."
 for i in {1..5}; do
   path="datasets/2003_td_dataset_discretized_10_bins_tube/Fold$i"
   echo "Running Fold $i..."
-  mkdir -p random_forest/predictions/discretized_10_bins_tube/fold${i}
+  mkdir -p random_forest/predictions/discretized_10_bins_tube/
   python $script_path $path/train.txt $path/test.txt &> random_forest/predictions/discretized_10_bins_tube/fold${i}
   echo ""
 done
@@ -28,7 +28,7 @@ echo "Running discretized uniform 20 bins..."
 for i in {1..5}; do
   path="datasets/2003_td_dataset_discretized_20_bins_uniform/Fold$i"
   echo "Running Fold $i..."
-  mkdir -p random_forest/predictions/discretized_20_bins_uniform/fold${i}
+  mkdir -p random_forest/predictions/discretized_20_bins_uniform/
   python $script_path $path/train.txt $path/test.txt &> random_forest/predictions/discretized_20_bins_uniform/fold${i}
   echo ""
 done
