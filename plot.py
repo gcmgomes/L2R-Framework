34c11f291fe6354e12370b96d0febca1162dbbe8
Thiago Silva <thiagovieiraas@gmail.com>
2017-04-27 15:50:53 -0300

    Implemented a script to print a pearson correlation of MAP by Score and plot a scatter plot.

--- plot.py
+++ plot.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Plotting the MAP X Score.
+# MAP on the Y axis.
+# Score on the X axis.
 
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.plotly as py
+from scipy.stats.stats import pearsonr
+import os.path
+
+bin_count=20
+
+for i in range(5):
+  for criterion in ["AIC", "BIC"]:
+    for query_number in range(50):
+      filename="results/" + criterion + "/2003_td_dataset_discretized_" + str(bin_count) + "_bins_uniform/Fold" + str(i) + "/query" + str(query_number)
+      if os.path.isfile(filename):
+        print "Fold " + str(i) + " " + criterion + " Query: " + str(query_number)
+        f = open(filename)
+        x = []
+        y = []
+        for line in f.readlines():
+          line = line.split()
+          y.append(float(line[0]))
+          x.append(float(line[2]))
+        
+        print pearsonr(x, y)
+        plt.plot(x, y, "o")
