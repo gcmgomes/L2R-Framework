258c0167d1587cdaecd3dd40ffd818134a618be1
Thiago V. de A. Silva <thiagovieiraas@gmail.com>
2017-04-27 16:08:32 -0300

    Fixed plot.py bug

--- plot.py
+++ plot.py
@@ -6,7 +6,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.plotly as py
 from scipy.stats.stats import pearsonr
 import os.path
 
@@ -15,16 +14,34 @@ bin_count=20
 for i in range(5):
   for criterion in ["AIC", "BIC"]:
     for query_number in range(50):
-      filename="results/" + criterion + "/2003_td_dataset_discretized_" + str(bin_count) + "_bins_uniform/Fold" + str(i) + "/query" + str(query_number)
+      filename="results/" + criterion + "/2003_td_dataset_discretized_" + str(bin_count) + "_bins_uniform/Fold" + str(i) + "/query" + str(query_number) + "/map_score.txt"
+      
+      
       if os.path.isfile(filename):
-        print "Fold " + str(i) + " " + criterion + " Query: " + str(query_number)
         f = open(filename)
         x = []
         y = []
+        mini = 12367875
+        maxi = -34567898
         for line in f.readlines():
           line = line.split()
           y.append(float(line[0]))
           x.append(float(line[2]))
+          maxi = max(maxi, float(line[2]))
+          mini = min(mini, float(line[2]))
+        f.close()
+        
+        
+        if len(x) == 1:
+          continue      
+        
+        print filename
+        print "Fold " + str(i) + " " + criterion + " Query: " + str(query_number) + " #ParentSets: " + str(len(x))
+        
+        for j in range(len(x)):
+          x[j] = (x[j]-mini)/(maxi-mini)
         
         print pearsonr(x, y)
         plt.plot(x, y, "o")
+        plt.show()
+        print ""
